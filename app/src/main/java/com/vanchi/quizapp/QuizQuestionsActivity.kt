738d936c59f394f84d675d0da400739e8203fdc4
vayipalayamchand <vayipalayamchand@paypal.com>
2021-11-05 11:30:45 -0500

    Adding validations for answers
    Added ability to finish the game.

--- app/src/main/java/com/vanchi/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/vanchi/quizapp/QuizQuestionsActivity.kt
@@ -1,11 +1,13 @@
 package com.vanchi.quizapp
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.vanchi.quizapp.databinding.ActivityQuizQuestionsBinding
@@ -28,17 +30,20 @@ class QuizQuestionsActivity: AppCompatActivity(), OnClickListener{
         binding.optionTwo.setOnClickListener(this)
         binding.optionThree.setOnClickListener(this)
         binding.optionFour.setOnClickListener(this)
+        binding.submitButton.setOnClickListener(this)
     }
 
     private fun QuizQuestionsActivity.setQuestion() {
         val question: Question? = mQuestionList!![mCurrentPostion - 1]
         binding.progressBar.progress = mCurrentPostion
+        binding.progressBar.max = mQuestionList!!.size
         binding.progressBarText.text = "${mCurrentPostion}/${binding.progressBar.max}"
         binding.questionImage.setImageResource(question!!.img)
         binding.optionOne.text = question.optionOne
         binding.optionTwo.text = question.optionTwo
         binding.optionThree.text = question.optionThree
         binding.optionFour.text = question.optionFour
+        binding.submitButton.text = "SUBMIT"
     }
 
     private fun defaultOptionsView(){
@@ -72,6 +77,54 @@ class QuizQuestionsActivity: AppCompatActivity(), OnClickListener{
             R.id.optionFour -> {
                 selectedOptionsView(binding.optionFour,4)
             }
+            R.id.submitButton -> {
+                if(mCurrentPostion > mQuestionList!!.size){
+                    val mainActivity:Intent = Intent(this, MainActivity::class.java)
+                    startActivity(mainActivity)
+                    finish()
+                }
+                if (mSelectedOptionPostion != 0) {
+
+                    if (mSelectedOptionPostion != mQuestionList!![mCurrentPostion - 1].correctAnswer) {
+                        answersView(mSelectedOptionPostion, R.drawable.wrong_option)
+                    }
+                    answersView(mQuestionList!![mCurrentPostion - 1].correctAnswer, R.drawable.correct_option)
+                    if (mCurrentPostion <= mQuestionList!!.size) {
+                        binding.submitButton.text = "GO TO NEXT QUESTION"
+                    }
+                    mSelectedOptionPostion = 0
+                }else {
+                    mCurrentPostion++
+                    when {
+                        mCurrentPostion <= mQuestionList!!.size -> {
+                            setQuestion()
+                            mSelectedOptionPostion = 0
+                    }else -> {
+                        Toast.makeText(this, "You have completed the Quiz", Toast.LENGTH_SHORT).show()
+                        binding.submitButton.text = "DONE"
+                    }
+                }
+
+                }
+            }
+        }
+    }
+
+    private fun answersView(selectedOptionNum: Int, drawableView: Int){
+        when(selectedOptionNum){
+            1 -> {
+                binding.optionOne.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            2 -> {
+                binding.optionTwo.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            3 -> {
+                binding.optionThree.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            4 -> {
+                binding.optionFour.background = ContextCompat.getDrawable(this, drawableView)
+            }
+
         }
     }
 
